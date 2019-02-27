1e2798789091a434541182da4b321c99c0e72203
Eddy Caldas <eddycaldas@live.com>
2019-02-26 20:41:15 -0700

    init reducer

---
+++ src/store/reducers/root.js
@@ -0,0 +1,14 @@
+initialState = {
+    workersValues: [],
+    selectedWorker: null
+};
+
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+};
+
+export default reducer;
